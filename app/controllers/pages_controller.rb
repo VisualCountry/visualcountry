79b672e3f6f556cce5badb268c67fbe21a116f3b
Amber Lee <amber.a.lee@gmail.com>
2014-11-09 16:08:31 -0500

    login,sign up, index file, fonts, bootstrap

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def creators
+  end
+  
+  def brands
+  end
+
+  def login
+  end
+
+  def signup
+  end  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+ root 'pages#home'
+
+   get 'content-creators' => 'pages#creators'
+   get 'brands-agencies' => 'pages#brands'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
