6df22b1ab052818811806864d27261d253fe8272
Amber Lee <amber.a.lee@gmail.com>
2014-11-16 13:45:11 -0500

    added facebook friend count and instagram media to profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,13 +15,9 @@ class User < ActiveRecord::Base
   has_many :clients
   accepts_nested_attributes_for :clients
 
-  def facebook_data
-    graph = Koala::Facebook::API.new(self.facebook_token)
-    graph.get_connections('me', 'feed')
-  end
 
   def facebook_follower_count
-    facebook_client.user['counts']['followed_by']
+    facebook_client.get_connections('me', 'friends').raw_response['summary']['total_count']
   end
 
   def instagram_follower_count
@@ -32,16 +28,18 @@ class User < ActiveRecord::Base
     instagram_client.user['counts']['follows']
   end
 
-  private
+  def instagram_media
+    instagram_client.user_recent_media
+    #    instagram_client.user_recent_media.first['images']['standard_resolution']['url']
+  end
+
+private
 
   def instagram_client
     @instagram_client ||= Instagram.client(:access_token => self.instagram_token)
   end
 
-  def instagram_recent_media 
-    instagram_client.user['']
-  end  
-
-
-
+  def facebook_client
+    @facebook_client ||= Koala::Facebook::API.new(self.facebook_token)
+  end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '1516230728630497', 'b5cab04a31993547c3ad19282f309fb1', scope: 'public_profile,read_stream'
+  provider :facebook, '1516230728630497', 'b5cab04a31993547c3ad19282f309fb1', scope: 'public_profile,read_stream,user_friends'
   provider :instagram, 'f5c2ab0447324f05b11f2b52dfd1da78', '688f123556084479a97cb6b605cdd815'
 end
