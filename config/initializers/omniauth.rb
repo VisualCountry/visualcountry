cb4e5f57745062fd66c5c171596ef21648ad7f97
Amber Lee <amber.a.lee@gmail.com>
2015-01-28 20:55:13 -0500

    Adds environment variables for omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '1516230728630497', 'b5cab04a31993547c3ad19282f309fb1', scope: 'public_profile,read_stream,user_friends'
-  provider :instagram, 'f5c2ab0447324f05b11f2b52dfd1da78', '688f123556084479a97cb6b605cdd815'
-  provider :twitter, '3qTH5woXpYBQeOWwIFFimyrQa', 'TU4u1Y7DENtuJfszTsuPgU4ljXFV2gnd6TLyRCNM1RYFkBbtP3'
-  provider :pinterest, '1441389', 'a6b3c1d5'
+  provider :facebook, ENV['FACEBOOK_CLIENT_ID'], ENV['FACEBOOK_CLIENT_SECRET'], scope: 'public_profile,read_stream,user_friends'
+  provider :instagram, ENV['INSTAGRAM_CLIENT_ID'], ENV['INSTAGRAM_CLIENT_SECRET']
+  provider :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_SECRET_KEY']
+  provider :pinterest, ENV['PINTEREST_CLIENT_ID'], ENV['PINTEREST_CLIENT_SECRET']
 end
