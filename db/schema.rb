e1826e959fd4f64889602984a713774c13c429ad
Amber Lee <amber.a.lee@gmail.com>
2015-01-31 13:55:37 -0500

    removing pictures from schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141117032418) do
+ActiveRecord::Schema.define(version: 20150131185251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,16 +34,6 @@ ActiveRecord::Schema.define(version: 20141117032418) do
   create_table "interests_users", force: true do |t|
     t.integer "interest_id"
     t.integer "user_id"
-  end
-
-  create_table "pictures", force: true do |t|
-    t.string   "title"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
-    t.datetime "image_updated_at"
   end
 
   create_table "presses", force: true do |t|
