3e95f6eaa77e563fd4a35575ec06a9698926d3de
Amber Lee <amber.a.lee@gmail.com>
2015-02-10 21:47:41 -0500

    Styling User#show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,11 +11,11 @@ module ApplicationHelper
       autobuffer: true,
       data: {setup:'{}'},
       id: unique_id(src),
-      class: options[:class] += 'video-js vjs-default-skin'
+      class: options[:class] += ' video-js vjs-default-skin'
     }
 
     content_tag :video, attributes do
-      content_tag :source, '', src: src, type: 'video/mp4'
+      tag :source, src: src, type: 'video/mp4'
     end
   end
 
