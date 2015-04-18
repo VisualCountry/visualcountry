3dc86dfeafbde5e4b298d8ee178aebddaf4978dd
Amber Lee <amber.a.lee@gmail.com>
2015-04-18 14:30:06 -0400

    fixes list form for unauthenticated users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @available_lists = current_user.lists_without(@user)
+    @available_lists = current_user.lists_without(@user) if current_user
   end
 end
