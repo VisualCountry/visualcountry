bd1ad231f27d789c5a89e0198f842e227bf93f80
Amber Lee <amber.a.lee@gmail.com>
2015-04-28 14:43:36 -0400

    deleted old search results scss

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,4 +1,4 @@
 class Picture < ActiveRecord::Base
-  has_attached_file :image, :styles => {:medium => "300x300>", :thumb => "100x100>" }, :default_url => "missing_:style.png"
+  has_attached_file :image, :styles => {:medium => "300x300>", :thumb => "100x100>" }, :default_url => "/missing_avatar.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
