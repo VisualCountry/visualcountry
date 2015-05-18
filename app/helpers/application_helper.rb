0810be6750411ff2158ff56f9283f6cbf3d8ae22
Ryan Mcgarvey <ryan@themcgarveys.io>
2015-05-18 17:10:58 -0400

    Correctly draws flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -58,4 +58,14 @@ module ApplicationHelper
       ""
     end
   end
+
+  def bootstrap_class_for flash_type
+    { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }.stringify_keys[flash_type] || flash_type.to_s
+  end
+
+  def flash_messages(opts = {})
+    flash.each do |msg_type, message|
+      yield bootstrap_class_for(msg_type), message
+    end
+  end
 end
