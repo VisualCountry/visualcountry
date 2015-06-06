3c3436c580c280da5b113ed0beb45f7029f61bff
Amber Lee <amber.a.lee@gmail.com>
2015-06-06 16:03:12 -0400

    Updating focus names

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,19 +7,9 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 ActiveRecord::Base.transaction do
-  Interest.create(name:'DIY')
-  Interest.create(name:'Fashion')
-  Interest.create(name:'Art/Design')
-  Interest.create(name:'Animals')
-  Interest.create(name:'Comedy')
-  Interest.create(name:'Gaming')
-  Interest.create(name:'Sports')
-  Interest.create(name:'Music')
-  Interest.create(name:'Lifestyle')
-  Interest.create(name:'Travel')
-  Interest.create(name:'Celebrity')
-  Interest.create(name:'Photography')
-  Interest.create(name:'Technology')
-  Interest.create(name:'Food')
-  Interest.create(name:'Curation')
+  interests = ["Animals", "Art/Design", "Celebrity", "Comedy", "Curation", "DIY", "Family Friendly", "Fashion", "Food", "Gaming", "Music", "News", "Photography", "Sports", "Technology", "Travel"]
+  interests.map { |interest| Interest.create(name: interest) }
+
+  focuses = ["Actor", "Animator", "Assistant", "Blogger", "Dancer", "Designer", "Director", "Editor", "Hair Stylist", "Illustrator", "Make Up Artist", "Model", "Photographer", "Production", "Set Designer", "Stylist", "Viner", "Youtuber", "Instagrammer", "Musician", "Writer", "Food Stylist", "Prop Stylist", "Artist", "Journalist", "Developer"]
+  focuses.map { |focus| Focus.create(name: focus) }
 end
--- lib/tasks/once_and_done/alter_interests.rake
+++ lib/tasks/once_and_done/alter_interests.rake
@@ -1,6 +1,28 @@
 namespace :once_and_done do
-  task alter_interests: :environment do
-    Interest.find_by(name: "Family").update(name: "Family Friendly")
-    Interest.find_by(name: "Friendly").destroy
+  task alter_focuses: :environment do
+    NEW_FOCUS_MAPPINGS = {
+      'Hair' => 'Hair Stylist',
+      'MUA' => 'Make Up Artist',
+      'Set' => 'Set Designer',
+      'Production' => 'Producer'
+    }
+
+    NEW_FOCUS_MAPPINGS.map do |old, new|
+      puts "Updating #{old} to #{new}"
+      Focus.find_by(name: old).update(name: new)
+    end
+
+    puts "Removing second stylist"
+    Focus.where(name: 'Stylist').last.destroy
+
+    puts "Removing vlogger"
+    Focus.find_by(name: 'Vlogger').destroy
+
+    new_focuses = ['Viner', 'Youtuber', 'Instagrammer', 'Musician', 'Writer', 'Food Stylist', 'Prop Sylist', 'Artist', 'Journalist', 'Developer']
+
+    new_focuses.each do |focus|
+      puts "Creating #{focus}"
+      Focus.create(name: focus)
+    end
   end
 end
