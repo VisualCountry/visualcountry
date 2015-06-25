9f288baf3ac14f8941019c35cb053b1c649ee7b9
Amber Lee <amber.a.lee@gmail.com>
2015-06-24 21:47:24 -0400

    Sets cache expiration for profile media methods

--- app/services/instagram_adapter.rb
+++ app/services/instagram_adapter.rb
@@ -8,12 +8,16 @@ class InstagramAdapter < BaseAdapter
   end
 
   def media
-    @media ||= Rails.cache.fetch("instagram-media-#{service_token}") do
+    @media ||= Rails.cache.fetch(media_cache_key, expires_in: 6.hours) do
       client.user_recent_media
     end
   end
 
   private
+
+  def media_cache_key
+    "instagram-media-#{service_token}-v2"
+  end
 
   def client
     @client ||= Instagram.client(access_token: service_token)
