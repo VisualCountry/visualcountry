3a45ca56cada36a7138b07d550841f880cc2fd60
Brooks Swinnerton <bswinnerton@gmail.com>
2015-10-18 16:28:55 -0500

    Temporarily adding back User associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ActiveRecord::Base
   include ProfileAttributesWarning
 
+  has_many :press, dependent: :destroy
+  has_many :list_memberships, dependent: :destroy
+  has_many :organization_memberships, dependent: :destroy
+  has_many :organizations, through: :organization_memberships
+  has_and_belongs_to_many :interests
+  has_and_belongs_to_many :focuses
+  has_and_belongs_to_many :clients
+
   has_many :influencer_lists, dependent: :destroy
   has_one :profile
 
@@ -12,6 +20,15 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :confirmable,
          omniauth_providers: [:facebook, :instagram, :twitter, :pinterest]
+
+  has_attached_file :picture,
+    default_url: 'missing.png',
+    styles: {
+      medium: '300x300#',
+      thumb: '50x50#'
+    }
+  crop_attached_file :picture
+  validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
 
   def create_profile
     profile || Profile.create(user_id: id)
