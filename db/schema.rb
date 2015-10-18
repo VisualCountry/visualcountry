50071084384b316283aeba50636b03bfdf66dc25
Brooks Swinnerton <bswinnerton@gmail.com>
2015-10-18 09:53:53 -0500

    Removing profile_id from InfluencerLists
    `Profile`s don't own lists, `User`s do.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151017171955) do
+ActiveRecord::Schema.define(version: 20151018144922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,7 +80,6 @@ ActiveRecord::Schema.define(version: 20151017171955) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "uuid"
-    t.integer  "profile_id"
   end
 
   add_index "influencer_lists", ["user_id"], name: "index_influencer_lists_on_user_id", using: :btree
