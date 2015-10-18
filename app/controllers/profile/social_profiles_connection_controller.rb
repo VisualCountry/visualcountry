1d5aafbf92eac9cf3c68f7c3c25640dfaaf63f75
Brooks Swinnerton <bswinnerton@gmail.com>
2015-10-17 20:02:59 -0500

    Fixes layout bug caused in 0fed3c8

--- app/controllers/profile/social_profiles_connection_controller.rb
+++ app/controllers/profile/social_profiles_connection_controller.rb
@@ -1,7 +1,7 @@
 class Profile::SocialProfilesConnectionController < ApplicationController
   before_action :authenticate_user!
 
-  layout :application_with_sidebar
+  layout 'application_with_sidebar'
 
   def show
     @profile = current_user.profile

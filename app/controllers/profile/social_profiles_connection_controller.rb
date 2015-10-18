VisualCountry/visualcountry

commit 0fed3c8711eea4f5d975d5e4b2288814bfffa82f

Pointing social media page to Profile and User

app/controllers/profile/social_profiles_connection_controller.rb
@@ -1,15 +1,9 @@
 class Profile::SocialProfilesConnectionController < ApplicationController
   before_action :authenticate_user!
-  before_action :warm_follower_count_cache
 
-  layout "application_with_sidebar"
+  layout :application_with_sidebar
 
   def show
-  end
-
-  private
-
-  def warm_follower_count_cache
-    current_user.warm_follower_count_cache
+    @profile = current_user.profile
   end
 end

app/views/profile/social_profiles_connection/show.html.haml
@@ -3,9 +3,9 @@
   .row.text-center
     %h2 Social Profiles
     %h3 Connect your accounts
-    - if current_user.total_follower_count.present?
+    - if @profile.total_follower_count.present?
       %dl.total-reach
-        %dd.total-reach-count= number_with_delimiter(current_user.total_follower_count)
+        %dd.total-reach-count= number_with_delimiter(@profile.total_follower_count)
         %dt Total Reach
   .row
     .col-md-8.col-md-offset-2
@@ -16,7 +16,7 @@
           %h4.social-network Vine
           - if current_user.vine_token?
             %p.social-follower-count
-              followed by: #{number_with_delimiter(current_user.vine_follower_count)}
+              followed by: #{number_with_delimiter(@profile.vine_follower_count)}
             = render "shared/social_connection_link", social_link: profile_link_vine_path
           - else
             = link_to "Connect", new_profile_link_vine_path, class: "btn btn-default"
@@ -25,7 +25,7 @@
           %h4.social-network Twitter
           - if current_user.twitter_token?
             %p.social-follower-count
-              followed by: #{number_with_delimiter(current_user.twitter_follower_count)}
+              followed by: #{number_with_delimiter(@profile.twitter_follower_count)}
             = render "shared/social_connection_link", social_link: profile_link_twitter_path
           - else
             = link_to "Connect", user_omniauth_authorize_path(:twitter), class: "btn btn-default"
@@ -34,7 +34,7 @@
           %h4.social-network Instagram
           - if current_user.instagram_token?
             %p.social-follower-count
-              followed by: #{number_with_delimiter(current_user.instagram_follower_count)}
+              followed by: #{number_with_delimiter(@profile.instagram_follower_count)}
             = render "shared/social_connection_link", social_link: profile_link_instagram_path
           - else
             = link_to "Connect", user_omniauth_authorize_path(:instagram), class: "btn btn-default"
@@ -43,7 +43,7 @@
           %h4.social-network Facebook
           - if current_user.facebook_token?
             %p.social-follower-count
-              followed by: #{number_with_delimiter(current_user.facebook_follower_count)}
+              followed by: #{number_with_delimiter(@profile.facebook_follower_count)}
             = render "shared/social_connection_link", social_link: profile_link_facebook_path
           - else
             = link_to "Connect", user_omniauth_authorize_path(:facebook), class: "btn btn-default"
@@ -52,7 +52,7 @@
           %h4.social-network Pinterest
           - if current_user.pinterest_token?
             %p.social-follower-count
-              followed by: #{number_with_delimiter(current_user.pinterest_follower_count)}
+              followed by: #{number_with_delimiter(@profile.pinterest_follower_count)}
             = render "shared/social_connection_link", social_link: profile_link_pinterest_path
           - else
             = link_to "Coming Soon", "#pinterest", class: "btn btn-default"

commit 1d5aafbf92eac9cf3c68f7c3c25640dfaaf63f75

Fixes layout bug caused in 0fed3c8

app/controllers/profile/social_profiles_connection_controller.rb
@@ -1,7 +1,7 @@
 class Profile::SocialProfilesConnectionController < ApplicationController
   before_action :authenticate_user!
 
-  layout :application_with_sidebar
+  layout 'application_with_sidebar'
 
   def show
     @profile = current_user.profile