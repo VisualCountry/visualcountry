92c4acb52b3c2d7415d3313583204d10307f4de3
Brooks Swinnerton <bswinnerton@gmail.com>
2015-10-29 00:59:32 -0400

    Adds ethnicity and gender enums

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -10,6 +10,22 @@ class Profile < ActiveRecord::Base
   has_many :organization_memberships, dependent: :destroy
   has_many :organizations, through: :organization_memberships
 
+  enum gender: {
+    'Female'  => 0,
+    'Male'    => 1,
+    'Other'   => 2,
+  }
+
+  enum ethnicity: {
+    'Hispanic or Latino'                        => 0,
+    'Native American'                           => 1,
+    'Asian'                                     => 2,
+    'Black or African American'                 => 3,
+    'Native Hawaiian or other Pacific Islander' => 4,
+    'White'                                     => 5,
+    'Other/prefer not to answer'                => 6,
+  }
+
   has_attached_file :picture,
     default_url: 'missing.png',
     styles: {
